07b006b75a3eadd04fd2c9c040669b184234715f
Jason Sturges <jason@jasonsturges.com>
2022-12-10 14:52:45 -0600

    Local development example

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    Use this to run a local development environment of the library for testing
+  </React.StrictMode>
+);
